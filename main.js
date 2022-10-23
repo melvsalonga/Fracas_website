dec9af07169c404cdde0df4431389124a7b5bfd7
Focilia <melvinsalonga2999@gmail.com>
2022-10-23 16:41:42 +0800

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// alert('hello');
+// const toTop = document.querySelector('#totop');
+// toTop.css('display','none');
+// $(window).on('scroll',function(){
+//     var pos =$(window).scrollTop();
+//     if(pos >300){
+//         toTop.fadeIn();
+//     }else{
+//         toTop.fadeOut();
+//     }
+// });
+// toTop.on('click',function(){
+//     $('html, body'  ).animate({scrollTop:'0'},'500');
+// });
